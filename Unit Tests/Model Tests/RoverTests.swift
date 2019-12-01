89b6300b1565c79d24157e9005ef1d94e7546b2c
Nathan Ansel <nathan3o4@gmail.com>
2019-12-01 10:22:55 -0600

    Adds initial unit tests

---
+++ Unit Tests/Model Tests/RoverTests.swift
@@ -0,0 +1,11 @@
+//
+//  RoverTests.swift
+//  Unit Tests
+//
+//  Created by Nathan Ansel on 12/1/19.
+//  Copyright © 2019 Nathan Ansel. All rights reserved.
+//
+
+import XCTest
+@testable import Mars_Explorer
+
